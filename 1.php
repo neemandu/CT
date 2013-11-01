c93895d4f371b5546b8a973dcfd937f59e2c76ab
neemandu <neemandu@gmail.com>
2013-11-01 16:18:07 +0200

    1

--- 1.php
+++ 1.php
@@ -1,4 +1,11 @@
-php iksjd fhksjdh kjsdh kfjshf
-fhfghfghf
-fghfghfghf
-hfgfgh
+<?php
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/xampp/');
+	exit;
+?>
+Something is wrong with the XAMPP installation :-(
