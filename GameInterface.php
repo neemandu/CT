db3e38817fbd2cbfdb3910b72664da08bfc568a5
Nimrod Karni <karnimrod@gmail.com>
2013-11-03 12:59:27 +0200

    nimrod game interface

---
+++ GameInterface.php
@@ -0,0 +1,26 @@
+<?php
+interface GameInterface{
+	
+	/**
+	 * add a new player to the game
+	 */
+	public function addPlayer();
+	
+	/**
+	 * send a proposal from p1 to p2  
+	 * @param Player $player1
+	 * @param Player $player2
+	 * @param Offer $offer
+	 */
+	public function offer($player1, $player2, $offer);
+		
+	/**
+	 * 
+	 * @param Player $player1
+	 * @param Player $player2
+	 * @param ToTransfer $ToTransfer
+	 */
+	public function transfer($player1, $player2,$ToTransfer);
+	
+}
+?>
