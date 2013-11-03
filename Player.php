aa6a1456c8c7a09371d3cd32e8283af7b2d1db4b
Dudi <neemandu@gmail.com>
2013-11-03 11:21:04 +0200

    Create Player.php

---
+++ Player.php
@@ -0,0 +1,37 @@
+<?php
+
+	class Player
+	{
+		// property declaration
+		private $redChips = 3;
+		private $blueChips = 3;
+		private $greenChips = 3;
+		private $Name = 3;
+	
+		function __construct() {
+			
+		}
+	
+		// method declaration
+		public function setName($Name) {
+			$this->Name = $Name;
+			if(strcmp($Name, "DUDI") != 0){
+				$this->redChips = 10;
+				$this->blueChips = 10;
+				$this->greenChips  = 10;
+			}
+		}
+		
+		public function getRedChips() {
+			return $this->redChips;
+		}
+	
+		public function getBlueChips() {
+			return $this->blueChips;
+		}
+	
+		public function getGreenChips() {
+			return $this->greenChips;
+		}
+	}
+?>
